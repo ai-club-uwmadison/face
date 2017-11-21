da6235b660a7b3b42c1d62fb5034b1c373844f99
Songwong Tasneeyapant <tasneeyapant@wisc.edu>
2017-11-21 13:19:25 -0600

    update face

--- face.py
+++ face.py
@@ -2,26 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-face_xml_path = '/Users/nat/anaconda/envs/py3_5/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
-#eye_xml_path = '/Users/nat/anaconda/envs/py3_5/share/OpenCV/haarcascades/haarcascade_eye.xml'
+frontFace_xml_path = 'haarcascade_frontalface_default.xml'
+sideFace_xml_path = 'haarcascade_profileface.xml'
 
-face_cascade = cv2.CascadeClassifier(face_xml_path)
-#eye_cascade = cv2.CascadeClassifier(eye_xml_path)
+frontFace_cascade = cv2.CascadeClassifier(frontFace_xml_path)
+sideFace_cascade = cv2.CascadeClassifier(sideFace_xml_path)
 
 target_size = (128, 128)
 
 def convertToRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def faceRetriver(img):
+def faceRetriever(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     plt.imshow(gray, cmap='gray')
 
-    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
-
     result = []
+
+    faces = frontFace_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=6, minSize=(50,50))
     for (x,y,w,h) in faces:
-        crop_img = convertToRGB(cv2.resize(img[y:y+h, x:x+w], target_size))
+        crop_img = cv2.resize(img[y:y+h, x:x+w], target_size)
+        result.append((crop_img, (x,y,w,h)))
+        
+    faces = sideFace_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=6, minSize=(50,50))
+    for (x,y,w,h) in faces:
+        crop_img = cv2.resize(img[y:y+h, x:x+w], target_size)
         result.append((crop_img, (x,y,w,h)))
 
     return result
